89148119aefe85f7f1385084b277a91a091e5b50
Oleksandra Nadtochii <alexandra.nadtochiy.v@gmail.com>
2018-04-04 21:30:55 +0300

    SignUp/Login functionality added

---
+++ mysite/post/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.TextField(max_length=30)
+    email = models.TextField(max_length=60)
+    email_confirmed = models.BooleanField(default=False)
+
+    # bio = models.TextField(max_length=500, blank=True)
+    # location = models.CharField(max_length=30, blank=True)
+    # birth_date = models.DateField(null=True, blank=True)
+    # country = models.CharField(max_length=30, blank=True)
+    # city = models.CharField(max_length=30, blank=True)
+
+    # def __str__(self):
+    #     return u'user: %s, email_confirmed: %s' % (self.user, self.email_confirmed)
+
+# @receiver(post_save, sender=User)
+# def update_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#     instance.profile.save()
