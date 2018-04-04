ee567bed157f3739c1cdbabd07522459a6efc806
Oleksandra Nadtochii <alexandra.nadtochiy.v@gmail.com>
2018-04-04 22:35:55 +0300

    Add view fixes

--- mysite/post/forms.py
+++ mysite/post/forms.py
@@ -1,9 +1,26 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
+    email = forms.EmailField(max_length=200)
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text=None,
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text=None,
+    )
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        help_texts = {
+            'username': None,
+            'email': None,
+        }
