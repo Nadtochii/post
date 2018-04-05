c7206ef5ac7bf7bc26ae4a482658abe3203b7e0f
Oleksandra Nadtochii <alexandra.nadtochiy.v@gmail.com>
2018-04-06 01:45:20 +0300

    Update settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -146,8 +146,10 @@ STATICFILES_DIRS = (
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'youremail@gmail.com'
-EMAIL_HOST_PASSWORD = 'yourpassword'
+# EMAIL_HOST_USER = 'youremail@gmail.com'
+# EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 
 LOGIN_REDIRECT_URL = 'home'
