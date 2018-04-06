932de68d95c7ac0299027544eff0ce1f1ca712b4
Oleksandra Nadtochii <alexandra.nadtochiy.v@gmail.com>
2018-04-06 09:25:21 +0300

    Undate main.js

--- mysite/static/main.js
+++ mysite/static/main.js
@@ -21,7 +21,7 @@ function create_post() {
             $('#post-text').val('');
             $('#post-title').val('');
             console.log(json); // log the returned json to the console
-            $("#talk").prepend("<li><a href=\"post/"+json.post_id+"><strong>"+json.title+"</strong></a><br>"+json.body+"</li>");
+            $("#talk").prepend("<li><a href=\"post/"+json.post_id+"\"><strong>"+json.title+"</strong></a><br>"+json.body+"</li>");
             console.log("success"); // another sanity check
         },
 
